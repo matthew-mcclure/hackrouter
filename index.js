0af2a40d2ed5863f6f95a070fdf8c73a65895e3d
matthew mcclure <mcclure0728@gmail.com>
2018-01-20 01:52:49 -0800

    adding mlabs mongoose connection

--- index.js
+++ index.js
@@ -3,6 +3,16 @@ const app = express()
 const port = process.env.PORT || 1000
 const routes = require('./routes')
 
+const mongoose = require('mongoose')
+const uristring = process.env.MONGODB_URI || 'mongodb://localhost/atthackathon';
+mongoose.connect(uristring, function (err, res) {
+    if (err) console.log (`ERROR connecting to: ${uristring}. Error: ${err}`);
+    else console.log (`Succeeded connected to: ${uristring}`);
+});
+
+
+
+
 app.use(routes)
 
 app.listen(port, () => console.log(`beep. boop. Scanning port number ${port}...`))
